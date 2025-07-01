23976b7534cceb437be383419f2a2e3a7b8bbe9f
Yatindra nath Pandey <142476080+yatindra2004@users.noreply.github.com>
2025-07-01 17:08:44 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
- origin:["http://localhost:5173" , "http://localhost:5174"],
+ origin:["https://shopnix.com/" , "http://localhost:5174"],
  credentials:true
 }))
 
